6b6659d4e62d7b3adb3f70a1d907cdfbe819df56
Савелий <Savka9765@yandex.ru>
2022-08-11 21:06:26 +0300

    Task 2. Программа, которая по заданному номеру четверти показывает диапозон возможных координат.

---
+++ Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y)*/
+
+void getCoordinateFromQuoter(int quoter)
+{
+    if (quoter == 1)
+    {
+        Console.WriteLine ("x > 0 и y > 0");
+    }
+    else if (quoter == 2)
+    {
+        Console.WriteLine ("x < 0 и y > 0");
+    }
+    else if (quoter == 3)
+    {
+        Console.WriteLine ("x < 0 и y < 0");
+    }
+    else if (quoter == 4)
+    {
+        Console.WriteLine ("x > 0 и y < 0");
+    }
+    else
+    {
+        Console.WriteLine ("Ошибка! Количество четвертей 4!");
+    }
+}
+
+Console.WriteLine ("Введите четверть: ");
+int quoter = Convert.ToInt32(Console.ReadLine());
+
+getCoordinateFromQuoter(quoter);
