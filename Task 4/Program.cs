cf8a692f5bd87f4e924901794e6496351e515f84
Савелий <Savka9765@yandex.ru>
2022-08-12 22:11:16 +0300

    Task 4. Программа, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.

---
+++ Task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+ 5 -> 1, 4, 9, 16, 25. 
+ 2 -> 1,4*/
+
+void table_of_squares(int initial_number, int user_number)
+{
+    Console.Write($"Таблица квадратов от 1 до {user_number}: ");
+    while (initial_number <= user_number)
+    {
+        int res = initial_number * initial_number;
+        initial_number++;
+        Console.Write($"{res} ");
+    }
+}
+
+Console.Write("Введите число: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+int initial_number = 1;
+table_of_squares(initial_number, user_number);
